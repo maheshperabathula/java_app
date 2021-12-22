c36653812efea50014da747699065320cbd8296c
maheshperabathula <maheshnaidu.perabathula>
2021-12-22 10:19:13 +0000

    added helloworld class

---
+++ helloword.java
@@ -0,0 +1,7 @@
+c public class helloworld {
+	public static void main (string[]args) {
+		system.out.println ("happy sankranthi");
+	        system.out.println ("happy sankranthi");
+	}
+}
+
---
+++ helloworld.java
@@ -0,0 +1,7 @@
+public class helloworld {
+	public static void main(string[] args) {
+		System.out.println("Happy sankranthi");
+		System.out.println("Happy sankranthi");
+	}
+}
+
